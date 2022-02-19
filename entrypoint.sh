a4801ba966957dca32810650593c8851bfab1d9f
Avinash Upadhyaya <avinashupadhya99@gmail.com>
2022-02-19 14:34:41 +0530

    Use shell for shebang and remove args

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-echo "Number of args $#\n"
-echo "args $2\n"
 echo "INPUT_ARGS $INPUT_ARGS"
 echo "INPUT_FILES $INPUT_FILES\n"
 echo "INPUT_CONTROL $INPUT_CONTROL\n"
 
-if [[ -nz "${INPUT_FRAMEWORK}" ]]; then
+if [[ ! -z "$INPUT_FRAMEWORK" ]]; then
 echo "Framework is set to $INPUT_FRAMEWORK"
 fi
