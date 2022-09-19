4f84d559deca9406e08ec9a2e039cbf159e57f69
Shivam Verma <vermashivams1510@gmail.com>
2022-09-19 19:07:12 +0530

    updated entrypoint.sh to scan repository and take inputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,42 @@
 #!/bin/sh
 
 set -e
-if [ "$INPUT_SCANREPOSITORY"  = "no" ]; then 
-  if [ ! -z "$INPUT_FRAMEWORK" ] && [ ! -z "$INPUT_CONTROL" ]; then
-  echo "Framework and Control is specified. Please specify either one of them or neither"
+if [ ! -z "$INPUT_FRAMEWORK" ] && [ ! -z "$INPUT_CONTROL" ]; then
+echo "Framework and Control is specified. Please specify either one of them or neither"
+exit 1
+fi
+
+# Split the controls by comma and concatenate with quotes around each control
+if [ ! -z "$INPUT_CONTROL" ]; then
+    CONTROLS=""
+    set -f; IFS=','
+    set -- $INPUT_CONTROL
+    set +f; unset IFS
+    for control in "$@"
+    do
+        control=$(echo $control | xargs) # Remove leading/trailing whitespaces
+        CONTROLS="$CONTROLS\"$control\","
+    done
+    CONTROLS=$(echo "${CONTROLS%?}")
+fi
+
+FRAMEWORK_CMD=$([ ! -z "$INPUT_FRAMEWORK" ] && echo "framework $INPUT_FRAMEWORK" || echo "")
+CONTROL_CMD=$([ ! -z "$INPUT_CONTROL" ] && echo "control $CONTROLS" || echo "")
+EXCEPTIONS_CMD=$([ ! -z "$INPUT_EXCEPTIONS"] && echo "--exceptions $INPUT_EXCEPTIONS" || echo "")
+
+if [ "$INPUT_SCANREPOSITORY"  = "no" ]; then
+  if [ -z "$INPUT_FILES" ]; then
+  echo "No files specified to scan. Please specify files to scan or scan the entire repository."
   exit 1
   fi
-
-  # Split the controls by comma and concatenate with quotes around each control
-  if [ ! -z "$INPUT_CONTROL" ]; then
-      CONTROLS=""
-      set -f; IFS=','
-      set -- $INPUT_CONTROL
-      set +f; unset IFS
-      for control in "$@"
-      do
-          control=$(echo $control | xargs) # Remove leading/trailing whitespaces
-          CONTROLS="$CONTROLS\"$control\","
-      done
-      CONTROLS=$(echo "${CONTROLS%?}")
+  COMMAND="kubescape scan $FRAMEWORK_CMD $CONTROL_CMD $INPUT_FILES --fail-threshold $INPUT_THRESHOLD $INPUT_ARGS $EXCEPTIONS_CMD"
+else
+  if [ -z "$INPUT_ACCOUNTGUID" ]; then
+  echo "Account id is not specified. Please provide the account id"
+  exit 1
+  else
+  COMMAND="kubescape scan https://github.com/$GITHUB_REPOSITORY $FRAMEWORK_CMD $CONTROL_CMD --fail-threshold $INPUT_THRESHOLD $INPUT_ARGS $EXCEPTIONS_CMD --submit --account=$INPUT_ACCOUNTGUID"
   fi
-
-  FRAMEWORK_CMD=$([ ! -z "$INPUT_FRAMEWORK" ] && echo "framework $INPUT_FRAMEWORK" || echo "")
-  CONTROL_CMD=$([ ! -z "$INPUT_CONTROL" ] && echo control $CONTROLS || echo "")
-
-  COMMAND="kubescape scan $FRAMEWORK_CMD $CONTROL_CMD $INPUT_FILES --fail-threshold $INPUT_THRESHOLD $INPUT_ARGS"
-
-  if [ ! -z "$INPUT_EXCEPTIONS" ]; then
-  COMMAND="${COMMAND} --exceptions ${INPUT_EXCEPTIONS}"
-  fi
-else 
-COMMAND="kubescape scan https://github.com/$GITHUB_REPOSITORY --submit --account=$INPUT_ACCOUNTGUID"
 fi
 
 eval $COMMAND
