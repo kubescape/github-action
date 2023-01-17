2e15cdf51e42015a98d209e5d46319c090b516d6
Vlad Klokun <vklokun@protonmail.ch>
2023-01-17 20:14:29 +0200

    feat: automatically add JSON to formats when fixes are requested

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,27 +16,11 @@ contains() {
 
 set -e
 
-# Declare Kubescape client
+# Kubescape uses the client name to make a request for checking for updates
 export KS_CLIENT="github_actions"
 
 if [ -n "${INPUT_FRAMEWORKS}" ] && [ -n "${INPUT_CONTROLS}" ]; then
     echo "Framework and Control is specified. Please specify either one of them or neither"
-    exit 1
-fi
-
-# Fixing files requires a JSON output file to be present
-have_json_format="false"
-if [ -n "${INPUT_FORMAT}" ] && contains "${INPUT_FORMAT}" "json"; then
-    have_json_format="true"
-fi
-
-should_fix_files="false"
-if [ "${INPUT_FIXFILES}" = "true" ]; then
-    should_fix_files="true"
-fi
-
-if [ "${should_fix_files}" = "true" ] && [ "${have_json_format}" != "true" ]; then
-    echo 'No output in JSON format to fix files. Autofixing files requires a JSON output file to be present. Please use "format: "json[,OTHER_FORMATS]"'
     exit 1
 fi
 
@@ -54,17 +38,30 @@ if [ -n "${INPUT_CONTROLS}" ]; then
     controls=$(echo "${controls%?}")
 fi
 
-# Subcommands
 frameworks_cmd=$([ -n "${INPUT_FRAMEWORKS}" ] && echo "framework ${INPUT_FRAMEWORKS}" || echo "")
 controls_cmd=$([ -n "${INPUT_CONTROLS}" ] && echo control "${controls}" || echo "")
 
-# Files to scan
 files=$([ -n "${INPUT_FILES}" ] && echo "${INPUT_FILES}" || echo .)
 
-# Output file name
+output_formats="${INPUT_FORMAT}"
+have_json_format="false"
+if [ -n "${output_formats}" ] && contains "${output_formats}" "json"; then
+    have_json_format="true"
+fi
+
+should_fix_files="false"
+if [ "${INPUT_FIXFILES}" = "true" ]; then
+    should_fix_files="true"
+fi
+
+# If a user requested Kubescape to fix their files, but forgot to ask for JSON
+# output, do it for them
+if [ "${should_fix_files}" = "true" ] && [ "${have_json_format}" != "true" ]; then
+    output_formats="${output_formats},json"
+fi
+
 output_file=$([ -n "${INPUT_OUTPUTFILE}" ] && echo "${INPUT_OUTPUTFILE}" || echo "results")
 
-# Command-line options
 account_opt=$([ -n "${INPUT_ACCOUNT}" ] && echo --account "${INPUT_ACCOUNT}" || echo "")
 
 # If account ID is empty, we load artifacts from the local path, otherwise we
@@ -73,13 +70,14 @@ artifacts_path="/home/ks/.kubescape"
 artifacts=$([ -n "${INPUT_ACCOUNT}" ] && echo "" || echo --use-artifacts-from "${artifacts_path}")
 
 fail_threshold_opt=$([ -n "${INPUT_FAILEDTHRESHOLD}" ] && echo --fail-threshold "${INPUT_FAILEDTHRESHOLD}" || echo "")
+
 severity_threshold_opt=$([ -n "${INPUT_SEVERITYTHRESHOLD}" ] && echo --severity-threshold "${INPUT_SEVERITYTHRESHOLD}" || echo "")
 
-# `kubescape fix` requires the latest "json" format version. Other formats
-# ignore this flag.
+# The `kubescape fix` subcommand requires the latest "json" format version.
+# Other formats ignore this flag.
 format_version_opt="--format-version v2"
 
-scan_command="kubescape scan $frameworks_cmd $controls_cmd $files $account_opt $fail_threshold_opt $severity_threshold_opt --format $INPUT_FORMAT $format_version_opt --output $output_file"
+scan_command="kubescape scan ${frameworks_cmd} ${controls_cmd} ${files} ${account_opt} ${fail_threshold_opt} ${severity_threshold_opt} --format ${output_formats} ${format_version_opt} --output ${output_file}"
 
 eval "${scan_command}"
 
